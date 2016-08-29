c81b7a1932744098fdef00c701d52829a522d4b0
solymosin <solymosi.norbert@gmail.com>
2016-08-29 16:22:27 +0200

    0.12

--- plugin/globalt_dialog.py
+++ plugin/globalt_dialog.py
@@ -2,7 +2,7 @@
 
 # Form implementation generated from reading ui file 'globalt_dialog_base.ui'
 #
-# Created: Mon Jun 13 06:16:33 2016
+# Created: Mon Aug 29 16:21:13 2016
 #      by: PyQt4 UI code generator 4.10.4
 #
 # WARNING! All changes made in this file will be lost!
